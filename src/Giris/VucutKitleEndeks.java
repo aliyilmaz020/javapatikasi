83a14205a3f2ddb61e8a355d34876ddd5a007edc
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-15 19:11:08 +0300

    ......

---
+++ src/Giris/VucutKitleEndeks.java
@@ -0,0 +1,18 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class VucutKitleEndeks {
+    public static void main(String[] args) {
+        int kilo;
+        double boy, indeks;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsi olarak (Örn: 1,64)) giriniz: ");
+        boy = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kilo = input.nextInt();
+        indeks = kilo/Math.pow(boy,2);
+        System.out.println("Vücut kitle indeksiniz: "+indeks);
+
+    }
+}
