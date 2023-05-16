bbd67bf8a3cbf0cb671a54288c87307b7fd6af23
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-16 18:03:02 +0300

    ......

---
+++ src/Giris2/Switch_Case.java
@@ -0,0 +1,17 @@
+package Giris2;
+
+public class Switch_Case {
+    public static void main(String[] args) {
+        int day =2;
+        switch (day){
+            case 1:
+                System.out.println("pzt");
+                break;
+            case 2:
+                System.out.println("salı");
+                break;
+            default:
+                System.out.println("hatali");
+        }
+    }
+}
