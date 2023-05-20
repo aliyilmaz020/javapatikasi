aef8dddecee5ca3a4466cc9b9290a85ea932d9c4
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-21 02:11:38 +0300

    ......

---
+++ src/Donguler/Combination.java
@@ -0,0 +1,24 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class Combination {
+    public static void main(String[] args) {
+        int sayi,kombSayi,sonuc,pay = 1,payda = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        sayi = sc.nextInt();
+        System.out.print("Kombinasyon sayısını giriniz: ");
+        kombSayi = sc.nextInt();
+
+        while(kombSayi>0){
+            pay *= sayi;
+            payda *= kombSayi;
+            sayi--;
+            kombSayi--;
+        }
+        sonuc = pay/payda;
+        System.out.println("Sonuç : " + sonuc);
+
+    }
+}
