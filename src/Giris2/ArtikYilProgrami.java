2b3b5cd23874668aea93ba728e02a1d221b1b452
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-20 15:06:52 +0300

    ......

---
+++ src/Giris2/ArtikYilProgrami.java
@@ -0,0 +1,30 @@
+package Giris2;
+import java.util.Scanner;
+
+public class ArtikYilProgrami {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Yıl giriniz: ");
+        int yil = input.nextInt();
+
+        boolean artikYil = false;
+
+        if (yil % 4 == 0) {
+            if (yil % 100 == 0) {
+                if (yil % 400 == 0) {
+                    artikYil = true;
+                }
+            } else {
+                artikYil = true;
+            }
+        }
+
+        if (artikYil) {
+            System.out.println(yil + " bir artık yıldır.");
+        } else {
+            System.out.println(yil + " bir artık yıl değildir.");
+        }
+    }
+}
+
