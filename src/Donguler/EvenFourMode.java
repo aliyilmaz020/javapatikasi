e6e347e4167850c05f57098c03f0ef988cc755ea
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-21 01:25:30 +0300

    ......

---
+++ src/Donguler/EvenFourMode.java
@@ -0,0 +1,19 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class EvenFourMode {
+    public static void main(String[] args) {
+        int sayi=0,toplam = 0;
+        Scanner input = new Scanner(System.in);
+        while (sayi%2==0){
+            System.out.print("Sayı Giriniz : ");
+            sayi = input.nextInt();
+            if(sayi % 2 == 0 && sayi % 4 == 0){
+                toplam += sayi;
+            }
+        }
+        System.out.println("Toplam : " + toplam);
+        
+    }
+}
