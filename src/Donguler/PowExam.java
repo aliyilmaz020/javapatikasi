06f851afb68f6bdb419ec313bb7af870fad82342
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-21 02:42:26 +0300

    ......

---
+++ src/Donguler/PowExam.java
@@ -0,0 +1,19 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class PowExam {
+    public static void main(String[] args) {
+        int sayi,us,sayac=1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        sayi = input.nextInt();
+        System.out.print("Üs giriniz: ");
+        us = input.nextInt();
+
+        for(int i = 1; i<=us;i++){
+            sayac *=sayi;
+        }
+        System.out.println("Sayının üssü: " + sayac);
+    }
+}
