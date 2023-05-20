77fc65edd47f09d88dfbf62320e353ae270db905
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-21 01:39:35 +0300

    ......

---
+++ src/Donguler/FourFivePow.java
@@ -0,0 +1,16 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class FourFivePow {
+    public static void main(String[] args) {
+        int sayi;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Sayı Giriniz : ");
+        sayi = sc.nextInt();
+        System.out.println("Durumlar  4^n 5^n\n------------------------");
+        for(int i = 0; i<=sayi; i++){
+            System.out.println(i+". Durum: " +(int)Math.pow(4,i) +"    "+(int)Math.pow(5,i) );
+        }
+    }
+}
