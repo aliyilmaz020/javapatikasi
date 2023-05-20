38a239c9e7c6ac315321eef6434f8c5cfb46a6fa
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-20 15:51:34 +0300

    ......

---
+++ src/Donguler/WhilePassword.java
@@ -0,0 +1,23 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class WhilePassword {
+    public static void main(String[] args) {
+        int password;
+        boolean isPasswordSuccess = false;
+        Scanner input = new Scanner(System.in);
+
+        while (!isPasswordSuccess) {
+            System.out.print("Şifre giriniz : ");
+            password = input.nextInt();
+
+            if (password == 123) {
+                System.out.println("Doğru");
+                isPasswordSuccess = true;
+            } else {
+                System.out.println("Yanlış");
+            }
+        }
+    }
+}
---
+++ src/Donguler/WhileTwo.java
@@ -0,0 +1,9 @@
+package Donguler;
+
+public class WhileTwo {
+    public static void main(String[] args) {
+        int left = 100,right = 200;
+        while (++left<--right);
+        System.out.println(left);
+    }
+}
