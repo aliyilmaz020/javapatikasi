666690d2ba92043b4620bf306da883e01aaf8269
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-21 17:48:12 +0300

    ......

---
+++ src/ElmasDeseni.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ElmasDeseni {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Elmasın yüksekliğini giriniz: ");
+        int yukseklik = scanner.nextInt();
+
+        int boslukSayisi = yukseklik - 1;
+        int yildizSayisi = 1;
+
+        // Üst kısım
+        for (int i = 0; i < yukseklik; i++) {
+            for (int j = 0; j < boslukSayisi; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < yildizSayisi; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            boslukSayisi--;
+            yildizSayisi += 2;
+        }
+
+        boslukSayisi = 1;
+        yildizSayisi = yildizSayisi - 4;
+
+        // Alt kısım
+        for (int i = 0; i < yukseklik - 1; i++) {
+            for (int j = 0; j < boslukSayisi; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < yildizSayisi; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            boslukSayisi++;
+            yildizSayisi -= 2;
+        }
+    }
+}
