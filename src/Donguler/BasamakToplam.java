642e799475203770f2ee51db78980449a5a4f584
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-21 17:18:47 +0300

    ......

---
+++ src/Donguler/BasamakToplam.java
@@ -0,0 +1,19 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class BasamakToplam {
+    public static void main(String[] args) {
+        int sayi,basamak,toplam = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz: ");
+        sayi = input.nextInt();
+
+        while (sayi>0){
+            basamak = sayi%10;
+            toplam += basamak;
+            sayi /= 10;
+        }
+        System.out.println(toplam);
+    }
+}
