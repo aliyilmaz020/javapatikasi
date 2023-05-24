f74dd5f68ad70a40fdcb72061dd8a7b39a903ca0
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-24 18:11:58 +0300

    ......

---
+++ src/Donguler/Fibonacci.java
@@ -0,0 +1,20 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Eleman Sayısı Giriniz: ");
+        int sayi  = input.nextInt();
+        int num1 = 0;
+        int num2 = 1;
+        int toplam;
+        for (int i = 0; i<=sayi;i++){
+            System.out.print(num1+" ");
+            toplam = num1+num2;
+            num1 = num2;
+            num2 = toplam;
+        }
+    }
+}
