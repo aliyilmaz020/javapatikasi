4474409dc3598026f5211cd8752ecc4edd899074
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-24 20:20:29 +0300

    ......

---
+++ src/Donguler/Exam.java
@@ -0,0 +1,22 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class Exam {
+    public static void main(String[] args) {
+        int sayac = 0,a,b,n;
+        Scanner input = new Scanner(System.in);
+        int q = input.nextInt();
+        for(int i = 0; i<q;i++){
+            a = input.nextInt();
+            b = input.nextInt();
+            n = input.nextInt();
+            for(int j = 0; j<n;j++){
+                sayac +=(int) Math.pow(2, j)*b;
+                System.out.print((a+sayac)+" ");
+            }
+            System.out.println();
+            sayac = 0;
+        }
+    }
+}
