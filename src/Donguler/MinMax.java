52caaff116444a87f693ab5ac7367e2b60bf43e8
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-24 15:03:14 +0300

    ......

---
+++ src/Donguler/MinMax.java
@@ -0,0 +1,33 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Kaç tane sayı gireceksiniz? N = ");
+        int n = scanner.nextInt();
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print((i + 1) + ". sayıyı girin: ");
+            int num = scanner.nextInt();
+
+            if (num < min) {
+                min = num;
+            }
+
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        System.out.println("En küçük sayı: " + min);
+        System.out.println("En büyük sayı: " + max);
+
+        scanner.close();
+    }
+}
