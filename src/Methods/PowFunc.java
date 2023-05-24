a1cf5dfcf9e32f0d0a15645ff8c236ed858e843f
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-24 23:43:23 +0300

    ......

---
+++ src/Methods/PowFunc.java
@@ -0,0 +1,28 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class PowFunc {
+    public static double Pow(double taban, int us){
+        double sonuc = 1;
+        if(us<0){
+            us=us* -1;
+            for(int i = 1; i<=us;i++){
+                sonuc *= (1.0/taban);
+            }
+        }else {
+            for (int i = 1;i<=us;i++){
+                sonuc *= taban;
+            }
+        }
+        return sonuc;
+    }
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        System.out.print("Tabanı giriniz: ");
+        int taban = input.nextInt();
+        System.out.print("Üssünü giriniz: ");
+        int us = input.nextInt();
+        System.out.println(Pow(taban,us));
+    }
+}
