4753d2de0950745ffd6ef40d029b888f55dfd65f
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-25 14:15:08 +0300

    ......

---
+++ src/Methods/Palindrom.java
@@ -0,0 +1,20 @@
+package Methods;
+
+public class Palindrom {
+    static void PalindromSayi(int sayi){
+        int temp = sayi, num = 0,sonNum = 0;
+        while(temp != 0){
+            sonNum = temp%10;
+            num  = (num*10) + sonNum;
+            temp /= 10;
+        }
+        if(sayi!=num){
+            System.out.println(sayi +" Palindrom Sayı değildir.");
+        }else{
+            System.out.println(sayi+" Palindrom Sayıdır.");
+        }
+    }
+    public static void main(String[] args) {
+        PalindromSayi(141);
+    }
+}
