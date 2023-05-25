af7c7f1704c764cae1cd8a6732db6211784dac9d
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-25 17:48:16 +0300

    ......

---
+++ src/Methods/PowRecursive.java
@@ -0,0 +1,16 @@
+package Methods;
+
+public class PowRecursive {
+    static int Pow(int a,int b){
+        if(b==0){
+            return 1;
+        }else {
+            return a *Pow(a,(b-1));
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = Pow(2,0);
+        System.out.println(num);
+    }
+}
