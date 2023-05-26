cea5355e3fcea469c2f1311467b9671af4c1a57c
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-26 16:14:56 +0300

    ......

---
+++ src/Methods/DesenMethod.java
@@ -0,0 +1,25 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class DesenMethod {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int n = scanner.nextInt();
+
+        DesenMethods(n);
+    }
+
+    public static void DesenMethods(int sayi) {
+        if (sayi > 0) {
+            System.out.println("Çıkarma işlemi: " + sayi);
+            DesenMethods(sayi - 5);
+        } else {
+            System.out.println("Toplama işlemi: " + sayi);
+            if (sayi < 0) {
+                DesenMethods(sayi + 5);
+            }
+        }
+    }
+}
