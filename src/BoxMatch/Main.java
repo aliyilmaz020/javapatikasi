0f2a4b9a50030e12c2cc5418eeb4d34200f1c505
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-05-29 16:27:58 +0300

    ......

---
+++ src/BoxMatch/Main.java
@@ -0,0 +1,11 @@
+package BoxMatch;
+
+public class Main {
+    public static void main(String[] args) {
+        Fighter f1 = new Fighter("Rambo",20,120,90,50);
+        Fighter f2 = new Fighter("Mohammed Ali", 20,120,100,50);
+
+        Match match = new Match(f1,f2,90,100);
+        match.run();
+    }
+}
