a3b442ce0346c0c69481a06731fcd148521c9988
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-11-24 15:37:19 +0300

    CharB

--- src/Arrays/CharBStar.java
+++ src/Arrays/CharBStar.java
@@ -1,18 +1,27 @@
 package Arrays;
 
+// 0 ve 5. sütunlarla başlayanlar *
+// 0, 3 ve 6. satırlarla başlayanlar ise *
+//geri kalanlar boşluk
 public class CharBStar {
     public static void main(String[] args) {
-        String[][] dizi = new String[6][4];
-        for (int i = 0; i < dizi.length; i++) {
-            for (int j = 0; j < dizi[i].length; j++) {
-                if (i == 0 || i == 2) {
-                    dizi[i][j] = "*";
-                } else if (j == 0 || j == 3) {
-                    dizi[i][j] = "*";
+        String[][] letter = new String[7][6];
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 5) {
+                    letter[i][j] = " * ";
                 } else {
-                    dizi[i][j] = " ";
+                    letter[i][j] = "   ";
                 }
             }
+        }
+        for (String row[] : letter) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
         }
     }
 }
