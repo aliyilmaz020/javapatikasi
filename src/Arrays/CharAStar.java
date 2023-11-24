3a42660d662a8beeed81e851759d09002f806872
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-11-24 15:18:00 +0300

    modified:   src/Arrays/CharAStar.java

--- src/Arrays/CharAStar.java
+++ src/Arrays/CharAStar.java
@@ -6,11 +6,11 @@ public class CharAStar {
     for (int i = 0; i < letter.length; i++) {
       for (int j = 0; j < letter[i].length; j++) {
         if (i == 0 || i == 2) {
-          letter[i][j] = " *";
+          letter[i][j] = " * ";
         } else if (j == 0 || j == 3) {
-          letter[i][j] = " *";
+          letter[i][j] = " * ";
         } else {
-          letter[i][j] = "  ";
+          letter[i][j] = "   ";
         }
       }
     }
--- src/Arrays/CharBStar.java
+++ src/Arrays/CharBStar.java
@@ -3,15 +3,13 @@ package Arrays;
 public class CharBStar {
     public static void main(String[] args) {
         String[][] dizi = new String[6][4];
-        for (int i = 0;i< dizi.length;i++){
-            for(int j = 0; j< dizi[i].length;j++){
-                if(i == 0 || i == 2){
+        for (int i = 0; i < dizi.length; i++) {
+            for (int j = 0; j < dizi[i].length; j++) {
+                if (i == 0 || i == 2) {
                     dizi[i][j] = "*";
-                }
-                else if (j==0 || j == 3){
-                    dizi[i][j] ="*";
-                }
-                else {
+                } else if (j == 0 || j == 3) {
+                    dizi[i][j] = "*";
+                } else {
                     dizi[i][j] = " ";
                 }
             }
