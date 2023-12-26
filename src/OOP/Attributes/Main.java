73d804bbfc331c6c41d1a9156a09cc30a4c04c60
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-12-26 19:17:43 +0300

    Constructor

--- src/OOP/Attributes/Main.java
+++ src/OOP/Attributes/Main.java
@@ -2,12 +2,12 @@ package OOP.Attributes;
 
 public class Main {
   public static void main(String[] args) {
-    Product product = new Product();
-    product.setName("Laptop");
-    product.setId(1);
-    product.setDescription("Asus");
-    product.setPrice(5000);
-    product.setStockAmount(5);
+    Product product = new Product(1, "Laptop", "Asus", 2000, 2, "Black");
+    // product.setName("Laptop");
+    // product.setId(1);
+    // product.setDescription("Asus");
+    // product.setPrice(5000);
+    // product.setStockAmount(5);
 
     ProductManager productManager = new ProductManager();
     productManager.Add(product);
--- src/OOP/Attributes/Product.java
+++ src/OOP/Attributes/Product.java
@@ -1,6 +1,21 @@
 package OOP.Attributes;
 
 public class Product {
+
+  public Product(int id, String name, String description, double price, int stockAmount, String renk) {
+    System.out.println("Yapıcı Blok Çalıştı");
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.stockAmount = stockAmount;
+    this.renk = renk;
+  }
+
+  public Product() {
+
+  }
+
   // attribute | field
   private int id;
   private String name;
