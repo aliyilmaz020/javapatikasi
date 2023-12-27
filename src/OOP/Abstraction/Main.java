de8cf2362cda0d57cef265b08d9abbcfb618ea55
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-12-27 13:33:16 +0300

    training

--- src/OOP/Abstraction/Main.java
+++ src/OOP/Abstraction/Main.java
@@ -2,12 +2,16 @@ package OOP.Abstraction;
 
 public class Main {
   public static void main(String[] args) {
-    WomanGameCalculator womanGameCalculator = new WomanGameCalculator();
-    womanGameCalculator.hesapla();
-    womanGameCalculator.gameOver();
+    // WomanGameCalculator womanGameCalculator = new WomanGameCalculator();
+    // womanGameCalculator.hesapla();
+    // womanGameCalculator.gameOver();
 
     GameCalculator gameCalculator = new WomanGameCalculator();
-
+    gameCalculator.hesapla();
+    gameCalculator.gameOver();
+    System.out.println("--------------------------");
+    GameCalculator gameCalculator2 = new OlderGameCalculator();
+    gameCalculator2.hesapla();
     // GameCalculator gameCalculator = new GameCalculator() {
 
     // @Override
--- src/OOP/Abstraction/OlderGameCalculator.java
+++ src/OOP/Abstraction/OlderGameCalculator.java
@@ -4,7 +4,7 @@ public class OlderGameCalculator extends GameCalculator {
 
   @Override
   public void hesapla() {
-
+    System.out.println("Puanınız: 76");
   }
 
 }
