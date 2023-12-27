6a93421ab8ed8cbf34b63e3880487af91a7aac02
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-12-27 16:04:45 +0300

    Composition, Inner Class, Static

---
+++ src/StaticDemo/ProductManager.java
@@ -0,0 +1,16 @@
+package StaticDemo;
+
+public class ProductManager {
+  public void add(Product product) {
+    // ProductValidator validator = new ProductValidator();
+    if (ProductValidator.isValid(product)) { // static ise kendisini kullanır
+
+      System.out.println("Eklendi.");
+    } else {
+      System.out.println("Ürün bilgileri geçersiz.");
+    }
+    // ProductValidator productValidator = new ProductValidator();
+    // productValidator.another();
+  }
+}
+// Constructer çalışması için new'lemek gerekir.
