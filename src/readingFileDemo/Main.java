149cb71c287113f5404b381538930ded26decbe6
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-12-30 23:52:02 +0300

    Check Expections

--- src/readingFileDemo/Main.java
+++ src/readingFileDemo/Main.java
@@ -6,19 +6,33 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     // try-catch yap ya da başkası
     // kullanacaksa FileNotFoundExpection
     // metodunu kullanmalı
     BufferedReader reader = null;
+    int total = 0;
+
     try {
       reader = new BufferedReader(new FileReader("D:\\javapatikasi\\src\\readingFileDemo\\sayilar.txt"));
-
+      String line = null;
+      while ((line = reader.readLine()) != null) {
+        total += Integer.valueOf(line);
+      }
+      System.out.println("Toplam = " + total);
     } catch (FileNotFoundException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     } finally {
-      reader.close();
+      try {
+        reader.close();
+      } catch (Exception e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
     }
 
   }
