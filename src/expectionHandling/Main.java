b05e99ed04356132c447e963a7fd6ebac697ea62
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2023-12-30 20:33:24 +0300

    ExpectionHiearchy demo

--- src/expectionHandling/Main.java
+++ src/expectionHandling/Main.java
@@ -4,16 +4,21 @@ public class Main {
   public static void main(String[] args) {
     try {// Çalıştırmayı dener
       int[] sayilar = new int[] { 1, 2, 3 };
-      System.out.println(sayilar[2]);
-    } catch (Exception exception) {// Hata yakalar
+      System.out.println(sayilar[4]);
+    } catch (StringIndexOutOfBoundsException exception) {// Hata yakalar
       System.out.println("Hata oluştu");
       System.out.println(exception);
+    } catch (ArrayIndexOutOfBoundsException exception) {
+      System.out.println(exception);
+    } catch (Exception exception) { // Başka bir hata olursa o hataya yönlendiriyor
+      System.out.println("Loglandı : " + exception);
     } finally {
       System.out.println("Ben her türlü çalışırım.");
     } // Database'i veya benzeri şeyleri kapatmak için kullanılıyor genelde
 
   }
 }
+//
 /**
  * Throwable : Expection - Error
  * Expection : Runtime - IO - SQL - AWT - Interrupted
