9706e25af56cddff0c260306948b8f4453a6ba60
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-02 02:29:21 +0300

    repository generic sınıf & kısıtlar

---
+++ src/repositoryDemo/CustomerDal.java
@@ -0,0 +1,24 @@
+package repositoryDemo;
+
+public class CustomerDal implements IEntityRepository<Customer> {
+
+  @Override
+  public void add(Customer entity) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'add'");
+  }
+
+  @Override
+  public void delete(Customer entity) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'delete'");
+  }
+
+  @Override
+  public void update(Customer entity) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'update'");
+  }
+
+}
+// generic constrains - generic kısıt
---
+++ src/repositoryDemo/Main.java
@@ -0,0 +1,10 @@
+package repositoryDemo;
+
+/**
+ * Repository, DB işlemleri için kullanılır
+ */
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
