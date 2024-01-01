c8d79c3bc99e010b0980381e13e128ceacc6ca9d
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-02 02:33:35 +0300

    generic methods

--- src/repositoryDemo/Main.java
+++ src/repositoryDemo/Main.java
@@ -5,6 +5,8 @@ package repositoryDemo;
  */
 public class Main {
   public static void main(String[] args) {
-
+    Validator validator = new Validator();
+    Customer customer = new Customer();
+    validator.validate(customer);
   }
 }
