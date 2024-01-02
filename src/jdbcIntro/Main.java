34ec7a3512c2314db845b4e19d2f12e4bb8113a6
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-03 01:27:07 +0300

    Sql Select Query

--- src/jdbcIntro/Main.java
+++ src/jdbcIntro/Main.java
@@ -1,15 +1,24 @@
 package jdbcIntro;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
   public static void main(String[] args) throws SQLException {
     Connection connection = null;
     DbHelper helper = new DbHelper();
+    Statement statement = null;
+    ResultSet resultSet;
     try {
       connection = helper.getConnection();
-      System.out.println("Bağlantı oluştu");
+      // System.out.println("Bağlantı oluştu");
+      statement = connection.createStatement();
+      resultSet = statement.executeQuery("select code,name,continent,region from country");
+      while (resultSet.next()) {
+        System.out.println(resultSet.getString("continent"));
+      }
     } catch (SQLException exception) {
       helper.showErrorMessage(exception);
     } finally {
