9508d2d7a7d3ab7cb363d939cd1c3b5e14b33cb5
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-03 01:21:40 +0300

    DbHelper

--- src/jdbcIntro/Main.java
+++ src/jdbcIntro/Main.java
@@ -1,22 +1,17 @@
 package jdbcIntro;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-  static String userName = "root";
-  static String password = "mysql";
-  static String dbUrl = "jdbc:mysql://localhost:3306/world";
-
   public static void main(String[] args) throws SQLException {
     Connection connection = null;
+    DbHelper helper = new DbHelper();
     try {
-      connection = DriverManager.getConnection(dbUrl, userName, password);
-
+      connection = helper.getConnection();
       System.out.println("Bağlantı oluştu");
     } catch (SQLException exception) {
-      System.out.println(exception.getMessage());
+      helper.showErrorMessage(exception);
     } finally {
       connection.close();
     }
