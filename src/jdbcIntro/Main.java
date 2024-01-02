7cc9db7351ffeeae2ebf95ebaa1b6a4b746e076b
aliyilmaz020 <aliyilmaz@ogr.bandirma.edu.tr>
2024-01-03 01:33:37 +0300

    SQL query execute

--- src/jdbcIntro/Main.java
+++ src/jdbcIntro/Main.java
@@ -9,15 +9,15 @@ public class Main {
   public static void main(String[] args) throws SQLException {
     Connection connection = null;
     DbHelper helper = new DbHelper();
-    Statement statement = null;
-    ResultSet resultSet;
+    Statement statement = null; // Sql cümlesi ile ilgili işlemleri yapar
+    ResultSet resultSet; // İşlem sonucundaki veriseti
     try {
       connection = helper.getConnection();
       // System.out.println("Bağlantı oluştu");
-      statement = connection.createStatement();
-      resultSet = statement.executeQuery("select code,name,continent,region from country");
-      while (resultSet.next()) {
-        System.out.println(resultSet.getString("continent"));
+      statement = connection.createStatement(); // Bağlantıdan statement oluşturdu
+      resultSet = statement.executeQuery("select code,name,continent,region from country"); // query i çalıştırır
+      while (resultSet.next()) { // Datayı tek tek geziyor
+        System.out.println(resultSet.getString("continent")); // continent sütunu yazdırır
       }
     } catch (SQLException exception) {
       helper.showErrorMessage(exception);
